d0b98a032440ee28e12830c11c628cbbe78d58fc
FardinDaDev <fardin0855@gmail.com>
2020-10-09 18:29:39 +0200

    ☁ Config Files & More

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/*
+TODO LIST:
+Produce a page to report the machine statuses of the last 24 hours in the database (e.g. January 7th).
+
+    The total net production for the machine (gross output minus scrap).
+    The percentage of scrap vs gross production.
+    The percentage of downtime for a machine.
+    A graph/table showing the net production (gross production – scrap) for every hour. 
+
+*/
+
+import express from "express";
+import mysql from "mysql";
+
