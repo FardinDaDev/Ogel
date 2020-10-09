6acaf31e3e6015caf346864b0be58ddcc0c93e4a
FardinDaDev <fardin0855@gmail.com>
2020-10-09 20:18:36 +0200

    Update files...

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 TODO LIST:
 Produce a page to report the machine statuses of the last 24 hours in the database (e.g. January 7th).
 
-    The total net production for the machine (gross output minus scrap).
+    The total net production for the machine (gross output - scrap).
     The percentage of scrap vs gross production.
     The percentage of downtime for a machine.
     A graph/table showing the net production (gross production – scrap) for every hour. 
@@ -10,4 +10,21 @@ Produce a page to report the machine statuses of the last 24 hours in the databa
 */
 
 import express from "express";
-import mysql from "mysql";
+import path from "path";
+import config from "../config.json";
+
+import "./db/index";
+
+const app = express();
+
+app.set("views", path.join(__dirname, "../../views"));
+
+app.set("view engine", "ejs");
+
+app.use('/static', express.static(path.join(__dirname, '../../static')))
+
+app.get("/", (_res, req) => { 
+    req.render(path.join(__dirname, "../../views/index"));
+});
+
+app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
